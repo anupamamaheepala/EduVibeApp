be3929bb437291bde953284062818725520ef21c
Himansa Dewnethmi Peiris <himansagunerathne@gmail.com>
2025-04-24 14:13:59 +0530

    AddCourseForm Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,15 +15,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 //import { AuthProvider } from './context/Context';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
+import CourseForm from './pages/courses/AddCourseForm'; // Added import for CourseForm
 
 function App() {
-  
   return (
     //<AuthProvider>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Signup" element={<Signup />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/add-course" element={<CourseForm />} /> {/* New route for CourseForm */}
         </Routes>
       </Router>
     //</AuthProvider>
