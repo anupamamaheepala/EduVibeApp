309fe91e6f028bae7d8c8bd7a8c8b39401853f4c
rashi21 <rashini059@gmail.com>
2025-04-25 21:05:11 +0530

    error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,10 @@ import { AuthProvider } from './pages/AuthContext';
 
 import Home from './pages/Home';
 import Signup from './pages/Signup';
-
-import UserDashboard from './pages/user/UserDashboard';
-
-
-
 import Login from './pages/Login';
 import CourseForm from './pages/courses/AddCourseForm';
 import AllCourses from './pages/courses/AllCourses';
 import UserDashboard from './pages/user/UserDashboard';
-
 
 import CheckImages from './components/CheckImages';
 import CommentSystem from './pages/comments/CommentSystem';
@@ -22,15 +16,10 @@ import AddPost from './pages/posts/AddPosts';
 import ViewPost from './pages/posts/ViewPosts';
 import UserPosts from './pages/posts/UserPosts';
 import EditUserPosts from './pages/posts/EditUserPosts';
-
-import DeleteUserPosts from './pages/posts/EditUserPosts';
-
-  const App = () => {
-
-import DeleteUserPosts from './pages/posts/EditUserPosts'; // If this is correct
+import DeleteUserPosts from './pages/posts/DeleteUserPosts'; // If this is correct
 
 const App = () => {
- return (
+  return (
     <AuthProvider>
       <Router>
         <Routes>
@@ -42,25 +31,16 @@ const App = () => {
           <Route path="/add-course" element={<CourseForm />} />
           <Route path="/dashboard" element={<UserDashboard />} />
 
-          
-  
-          <Route path="/CheckImages" element={<CheckImages />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Comment" element={<CommentSystem />} />
-          <Route path="/AddPost" element={<AddPost />} />
-
-
           {/* Posts and Comments */}
           <Route path="/check-images" element={<CheckImages />} />
           <Route path="/comment" element={<CommentSystem />} />
           <Route path="/add-post" element={<AddPost />} />
-
           <Route path="/posts" element={<ViewPost />} />
           <Route path="/UserPosts" element={<UserPosts />} />
           <Route path="/EditUserPosts/:id" element={<EditUserPosts />} />
           <Route path="/DeleteUserPosts/:id" element={<DeleteUserPosts />} />
 
-         />
+
         </Routes>
       </Router>
     </AuthProvider>
