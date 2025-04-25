388caaa6152306e8153bf95f01a62466fbd14eae
Himansa Dewnethmi Peiris <himansagunerathne@gmail.com>
2025-04-25 11:36:27 +0530

    second commit of user dashboard

--- frontend/src/pages/user/UserDashboard.jsx
+++ frontend/src/pages/user/UserDashboard.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import Sidebar from './Sidebar';
+import Header from '../Header';
+import Footer from '../Footer';
 import './../../css/user/UserDashboard.css';
 
 const UserDashboard = () => {
   return (
-    <div className="dashboard-container">
-      <Sidebar />
-      <main className="dashboard-content">
-        <h1>User Dashboard</h1>
-        <p>Welcome to your dashboard! This is a placeholder for your content.</p>
-      </main>
-    </div>
+    <>
+      <Header/>
+          <div className="dashboard-container">
+        <Sidebar />
+        <main className="dashboard-content">
+          <h1>User Dashboard</h1>
+          <p>Welcome to your dashboard! This is a placeholder for your content.</p>
+        </main>
+      </div>
+      <Footer />
+    </>
   );
 };
 
