74bed1308f63b3e02e640fb89d6736f7993d00e6
Himansa Dewnethmi Peiris <himansagunerathne@gmail.com>
2025-05-10 17:29:47 +0530

    fixed the frontend css files structure of Himansa's content

--- frontend/src/pages/user/UserRoutes.jsx
+++ frontend/src/pages/user/UserRoutes.jsx
@@ -6,15 +6,10 @@ import MyCourses from './MyCourses';
 import CourseDetails from './CourseView'; // Renamed from CourseView for clarity
 import MyPosts from '../posts/UserPosts';
 import SharedWithMe from '../posts/SharedPosts';
-
 import LearningPlans from './LearningPlans';
 import CourseChapters from './CourseChapters';
-
 import NotificationSystem from '../notifictions/NotificationSystem';
-// import LearningPlan from './LearningPlan';
 import Groups from './Groups';
-// import Settings from './Settings';
-
 
 const UserRoutes = () => {
   return (
@@ -25,21 +20,11 @@ const UserRoutes = () => {
         <Route path="mycourses" element={<MyCourses />} />
         <Route path="MyPosts" element={<MyPosts />} />
         <Route path="SharedWithMe" element={<SharedWithMe />} />
-
         <Route path="course/:courseId" element={<CourseDetails />} />
         <Route path="learning-plan" element={<LearningPlans />} />
         <Route path="course-chapters/:courseId" element={<CourseChapters />} />
-
-
         <Route path="Groups" element={<Groups />} />
-
-        <Route path="course/:courseId" element={<CourseView />} />
-
-        <Route path="Notifications" element={<NotificationSystem />}/>
-
-        {/* <Route path="learning-plan" element={<LearningPlan />} />
-        <Route path="settings" element={<Settings />} /> */}
-
+        <Route path="Notifications" element={<NotificationSystem />} />
       </Route>
     </Routes>
   );
